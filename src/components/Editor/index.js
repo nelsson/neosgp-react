71f2c3fb3e6cc4e239785b507a87fb34bac05676
Nelson Villafuerte O <nelsson@gmail.com>
2021-04-13 17:06:46 -0500

    test editor

--- src/components/Editor/index.js
+++ src/components/Editor/index.js
@@ -1,24 +1,36 @@
-import React, { Fragment, useEffect} from 'react';
-import { useQuill } from 'react-quilljs';
-import 'quill/dist/quill.snow.css'; 
-import {  } from 'react';
+import React, { Fragment, useEffect } from "react";
+import { useQuill } from "react-quilljs";
+import "quill/dist/quill.snow.css";
+import {} from "react";
+import { Base64 } from "js-base64";
 
 const EditorTemplate = () => {
-   const { quill, quillRef } = useQuill();
-   useEffect(() => {
-      if (quill) {
-        quill.clipboard.dangerouslyPasteHTML('<h1>React Hook for Quill!</h1>');
-      }
-    }, [quill]);
+  const { quill, quillRef } = useQuill();
+  useEffect(() => {
+    if (quill) {
+      quill.clipboard.dangerouslyPasteHTML("<h1>React Hook for Quill!</h1>");
+      quill.on("text-change", (e) => {
+        console.log("Text change!", e);
+        console.log("quillRef", quillRef.current);
 
-   return (
-      <Fragment>
-         <h2>Probando</h2>
-         <div style={{ width: '100%', height: 300 }}>
-            <div ref={quillRef} />
-         </div>
-      </Fragment>
-   );
-}
- 
+        var html = quillRef.current.children[0].innerHTML;
+        const dataB = Base64.encode(html);
+        const dataDecode = Base64.decode(dataB);
+        //   console.log("html", html);
+        //   console.log("html2", dataB);
+        //   console.log("html3", dataDecode);
+      });
+    }
+  }, [quill, quillRef]);
+
+  return (
+    <Fragment>
+      <h2>Probando</h2>
+      <div style={{ width: "100%", height: 300 }}>
+        <div ref={quillRef} />
+      </div>
+    </Fragment>
+  );
+};
+
 export default EditorTemplate;
